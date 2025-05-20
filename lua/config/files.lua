e28cf56265f862d1f17f6dc0b29c1d9f21a508f3
Chris McCabe <chris.mccabe@neurovalens.com>
2025-05-20 10:53:21 +0100

    feat: Adding select hidden files to Telescope

--- lua/config/files.lua
+++ lua/config/files.lua
@@ -7,3 +7,21 @@ vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
     vim.opt_local.statuscolumn = ""
   end,
 })
+
+local ignore_filetypes_list = {
+    "venv", "__pycache__", "%.xlsx", "%.jpg", "%.png", "%.webp",
+    "%.pdf", "%.odt", "%.ico", "%.docx", "%.pptx", "%.mp4", ".git/",
+}
+
+-- Enable hidden files on Telescope
+local telescope = require('telescope')
+  telescope.setup {
+    defaults = {
+      file_ignore_patterns = ignore_filetypes_list,
+    },
+    pickers = {
+      find_files = {
+        hidden = true,
+    },
+  },
+}
